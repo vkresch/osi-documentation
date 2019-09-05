84f77631619075b8e8649f64441fd22d96681e7c
Viktor Kreschenski <viktor.kreschenski@altran.com>
2019-09-05 10:36:40 +0200

    Trying virtualenv

--- conf.py
+++ conf.py
@@ -3,7 +3,7 @@
 import os
 import sys
 # sys.path.insert(0, os.path.abspath('.'))
-sys.path.append('/home/travis/build/OpenSimulationInterface/osi-validation')
+sys.path.append('/home/travis/build/osi-documentation/osi-validation')
 
 # -- Project information -----------------------------------------------------
 
